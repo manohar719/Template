2d3a4fcd6ef108ab2b4cceb4b340a0a375eb4c2f
manohar719 <chennamanohar719@gmail.com>
2018-05-07 20:10:36 +0530

    contact us

--- js/main.js
+++ js/main.js
@@ -17,6 +17,15 @@ jQuery(function($) {
 
             }
         }
+        $(".mat-input").focus(function(){
+  $(this).parent().addClass("is-active is-completed");
+});
+
+$(".mat-input").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("is-completed");
+  $(this).parent().removeClass("is-active");
+})
 
     });
     // accordian
